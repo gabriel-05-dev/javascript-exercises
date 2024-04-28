c7b9149861ba6169c2a7c6076837635da079e82c
gabriel-05-dev <g.luchita@protonmail.com>
2024-04-28 22:09:28 +0100

    Finished task 7, 8, 9,

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,46 @@
-const add = function() {
+const add = function(value1, value2) {
+	return (value1+value2);
+};
+
+const subtract = function(value1, value2) {
+	return (value1-value2);
+};
+
+const sum = function(valuesArray) {
+  let max_sum = 0;
+	if (valuesArray.length === 0) {
+    return 0
+  } else if (valuesArray.length === 1) {
+    return valuesArray[0];
+  } else if (valuesArray.length > 1) {
+    for (let i = 0; i < valuesArray.length; i++) {
+      max_sum += valuesArray[i];
+    }
+  }
+  return max_sum
+};
+
+const multiply = function(valuesArray) {
+ let answer = 1;
+ for (let i=0; i <valuesArray.length; i++) {
+  answer *= valuesArray[i];
+ }
+ return answer;
+};
+
+const power = function(value1, value2) {
+  return (value1**value2)
 	
 };
 
-const subtract = function() {
-	
-};
-
-const sum = function() {
-	
-};
-
-const multiply = function() {
-
-};
-
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
-};
+const factorial = function(value) {
+  let total = 1;
+  while (value > 1) {
+    total = total*value
+    value--
+  }
+  return total
+}
 
 // Do not edit below this line
 module.exports = {
--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,18 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    string = string.toLowerCase();
+    let word = string.split('');
+    let palindrome = string.split('');
+    palindrome = palindrome.reverse().join("")
+    palindrome = palindrome.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"").replace(/\s+/g, '')
+    word = word.join("")
+    word = word.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"").replace(/\s+/g, '')
+    if (word === palindrome) {
+        return true
+    } else {
+        return false
+    }
 };
 
 // Do not edit below this line
 module.exports = palindromes;
+
